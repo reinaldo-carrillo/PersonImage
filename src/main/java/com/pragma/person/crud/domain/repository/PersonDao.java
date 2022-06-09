421ab397d100c82001dae6a6c037b1fc044f715d
Reinaldo José Carrillo Roldán <reinaldo.carrillo@pragma.com.co>
2022-06-09 15:47:34 -0500

    FEAT: Capa domain, pendiente el update

--- src/main/java/com/pragma/person/crud/domain/repository/PersonDao.java
+++ src/main/java/com/pragma/person/crud/domain/repository/PersonDao.java
@@ -1,5 +1,20 @@
 package com.pragma.person.crud.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.pragma.person.crud.domain.model.PersonEntity;
+
 public interface PersonDao {
 
+	Optional<PersonEntity> getPersonById (Long id);
+	Optional<PersonEntity> getPersonByTypeAndDocumentNumber (String docType, int documentNumber);
+	void addPersonToEntity (PersonEntity personEntity);
+	void updatePersonFromEntity (Long id, PersonEntity personEntity);
+	void deletePersonByDocument (int documentNumber);
+	void deletePersonById (Long id);
+	List<PersonEntity> getAllPeople ();
+	List<PersonEntity> searchByAgeRange (int ageBottom, int ageTop);
+	List<PersonEntity> searchByOldestAge (int age);
+	
 }
--- src/main/java/com/pragma/person/crud/domain/repository/PersonRepository.java
+++ src/main/java/com/pragma/person/crud/domain/repository/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.pragma.person.crud.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,17 +13,22 @@ import com.pragma.person.crud.domain.model.PersonEntity;
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long>{
 
-	@Transactional
+	PersonEntity findByDocumentNumber (int documentNumber);
+	//@Transactional
 	@Modifying
-	@Query("SELECT * FROM buyer_tbl p WHERE p.cod_doc_type = ?1 AND p.document_number = ?2 ")
+	@Query(value="SELECT * FROM buyer_tbl AS p WHERE p.cod_doc_type = ?1 AND p.document_number = ?2", nativeQuery = true)
 	PersonEntity findByTypeAndDocumentNumber (String docType, int documentNumber);
+	//@Transactional
+	@Modifying
+	@Query(value="SELECT * FROM buyer_tbl AS p WHERE p.age >= ?1 AND p.age <= ?2", nativeQuery = true)
+	List<PersonEntity> findByAgeRange(int ageBottom, int ageTop);
+	//@Transactional
+	@Modifying
+	@Query(value="SELECT * FROM buyer_tbl AS p WHERE p.age >= ?1", nativeQuery = true)
+	List<PersonEntity> findByOldestAges(int age);
 	@Transactional
 	@Modifying
-	@Query("SELECT * FROM buyer_tbl p WHERE p.age >= ?1 AND p.age <= ?2 ")
-	PersonEntity findByAgeRange(int numberBottom, int numberTop);
-	@Transactional
-	@Modifying
-	@Query("SELECT * FROM buyer_tbl p WHERE p.age >= ?1")
-	PersonEntity findByAgeAdult(int age);
+	@Query(value="DELETE FROM buyer_tbl AS p WHERE p.document_number = ?1", nativeQuery = true)
+	void deletePersonByDocumentNumber(int documentNumber);
 	
 }
